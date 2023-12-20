1ec610f80f9b06f79d38fbb6691055bbeb91aa3f
feixiangdejiahao <714634079@qq.com>
2023-12-20 16:48:40 +0800

    update

---
+++ run_csmith.sh
@@ -0,0 +1,7 @@
+NUM_TESTS=1
+PARALLEL=1
+MUTATION_NUMBER=1000
+OUTDIR=output/
+
+rm -rf $OUTDIR
+seq -w 1 $NUM_TESTS | xargs -n 1 -P $PARALLEL -I {} bash -c "python3 main.py $OUTDIR {} $MUTATION_NUMBER csmith"
