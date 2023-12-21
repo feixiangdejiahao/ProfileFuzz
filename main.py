36f6ca4fd8501eb256ccad906362ad9d6cf21c72
feixiangdejiahao <714634079@qq.com>
2023-12-21 10:39:03 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from utils import *
 def main():
     if len(sys.argv) != 4:
         print("Usage: python3 main.py <dir_path> <file_name> <mutation_number> <generator>")
+        print(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+        exit(-1)
     dir_path = sys.argv[1]
     file_name = sys.argv[2]
     mutation_number = int(sys.argv[3])
