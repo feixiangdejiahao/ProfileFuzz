557476f849fe894b3ba08b353512ef7739986353
feixiangdejiahao <714634079@qq.com>
2023-12-25 12:54:45 +0800

    Update debug.py

--- debug.py
+++ debug.py
@@ -1,22 +1,27 @@
 import os
 import random
+import sys
 
+import utils
 from GcdaInfo import GcdaInfo, GCovDataFunctionAnnouncementRecord
+from GcnoInfo import GcnoInfo
 
-gcda = GcdaInfo()
-gcda.load("output/test1/test1.gcda")
-gcda.pull_records()
-records = gcda.records
-function_index = [records.index(record) for record in records if
-                  isinstance(record, GCovDataFunctionAnnouncementRecord)]
-index = random.choice(function_index)
-function = records[index]
-counters = records[index + 1]
 
-i = 0
-while i <= 10:
-    gcda.extremum_mutation(counters)
-    gcda.one_value_mutation(counters)
-    i += 1
-gcda.save("output/test1/test1_mut-test1.gcda")
-a = 1
+def modify_gcda(gcda_name, index, counter):  # index: the index of the function in gcda file
+    gcda = GcdaInfo()
+    gcda.load(gcda_name)
+    gcda.pull_records()
+    records = gcda.records
+    function_index = [records.index(record) for record in records if
+                      isinstance(record, GCovDataFunctionAnnouncementRecord)]
+    function_index = function_index[index]
+    records[function_index + 1].counters = counter
+    gcda.save(gcda_name)
+
+
+if len(sys.argv) != 4:
+    print("Usage: python3 debug.py <gcda_name> <index> <counter>")
+gcda_name = sys.argv[1]
+index = int(sys.argv[2])
+counter = eval(sys.argv[3])
+modify_gcda(gcda_name, index, counter)
